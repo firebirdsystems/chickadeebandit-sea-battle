1b6c3cc8daed0bbd07a2d12bd3890f78ed295a5c
firebirdsystems <admin@firebirdsystems.net>
2026-06-24 15:43:50 -0600

    Updated row_policies for data lock down and integrity

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -25,13 +25,14 @@ CREATE TABLE IF NOT EXISTS app_sea_battle__sea_battle_ships (
 );
 
 CREATE TABLE IF NOT EXISTS app_sea_battle__sea_battle_shots (
-  id            TEXT    NOT NULL,
-  game_id       TEXT    NOT NULL,
-  shooter_id    TEXT    NOT NULL,
-  target_row    INTEGER NOT NULL,
-  target_col    INTEGER NOT NULL,
-  result        TEXT    NOT NULL,
-  sunk_ship_type TEXT,
-  shot_at       TEXT    NOT NULL,
+  id               TEXT    NOT NULL,
+  game_id          TEXT    NOT NULL,
+  shooter_id       TEXT    NOT NULL,
+  target_player_id TEXT    NOT NULL,
+  target_row       INTEGER NOT NULL,
+  target_col       INTEGER NOT NULL,
+  result           TEXT    NOT NULL,
+  sunk_ship_type   TEXT,
+  shot_at          TEXT    NOT NULL,
   PRIMARY KEY (id)
 );
